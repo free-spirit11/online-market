0a520067c6e36909aee0b200d9def1660c58bcb5
Kovalchuk Serhii <free_spirit11@icloud.com>
2024-02-26 16:14:51 -0500

    started implementing stripe payments

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 import { useSelector } from 'react-redux';
 import { selectCartItems } from '../../store/cart/cart.selector';
 import { selectCartTotal } from '../../store/cart/cart.selector';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 const Checkout = () => {
     const cartItems = useSelector(selectCartItems);
@@ -31,6 +32,7 @@ const Checkout = () => {
                 return <CheckoutItem key={cartItem.id} cartItem={cartItem} />
             })}
             <span className='total'>Total: ${cartTotal}</span>
+            <PaymentForm />
         </div>
     );
 };
